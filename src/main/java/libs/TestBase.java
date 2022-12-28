1a4773840299a92d31003b65f3cd069ab17a97b8
rgajjar12 <80737974+rgajjar12@users.noreply.github.com>
2022-12-28 17:02:14 -0500

    Update TestBase.java

--- src/main/java/libs/TestBase.java
+++ src/main/java/libs/TestBase.java
@@ -4,5 +4,11 @@ public class TestBase {
 
 	public void test() {
 		System.out.println("this is demo project for GIT repo");
+		
+	}
+	
+	public void test2() {
+		System.out.println("this is second method");
+		
 	}
 }
